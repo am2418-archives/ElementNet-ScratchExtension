5e5538368933acb6bd49dfd6c810e97a772acb3a
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-28 13:59:06 -0500

    add some c blocks

--- extension.js
+++ extension.js
@@ -16,7 +16,12 @@ var htmlCode = "";
             // Block type, block name, function name
             [' ', '!DOCTYPE html', 'doctype'],
             [' ', 'text %s', 'txt','hi'],
-            [' ', 'compile HTML %s', 'comp', '']
+            [' ', 'compile HTML %s', 'comp', ''],
+            ['c', 'html', 'html'],
+            ['c', 'body', 'body'],
+            ['c', 'head', 'head'],
+            ['c', 'p', 'p'],
+            ['c', 'title', 'title']
         ],
 menus: {
 txt: ['hi','bye','how are you?'],
@@ -34,6 +39,31 @@ geo: ['latitude','longitude']
     }
         ext.comp = function(code) {
     location.assign("javascript:innerHTML='"+code+"';");
+    }
+            ext.html = function(i) {
+    htmlCode = htmlCode + "<html>";
+                i();
+           htmlCode = htmlCode + "</html>";     
+    }
+            ext.body = function(i) {
+    htmlCode = htmlCode + "<body>";
+                i();
+           htmlCode = htmlCode + "</body>";     
+    }
+            ext.head = function(i) {
+    htmlCode = htmlCode + "<head>";
+                i();
+           htmlCode = htmlCode + "</head>";     
+    }
+            ext.p = function(i) {
+    htmlCode = htmlCode + "<p>";
+                i();
+           htmlCode = htmlCode + "</p>";     
+    }
+            ext.title = function(i) {
+    htmlCode = htmlCode + "<title>";
+                i();
+           htmlCode = htmlCode + "</title>";     
     }
     // Name of Scratch Extension goes here
     ScratchExtensions.register('ElementNet', descriptor, ext); 
